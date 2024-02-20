a4a4caecc11b212be0febde08bcbbb38d8f27f9e
Gianluca Maida <124819060+Giaan46@users.noreply.github.com>
2024-02-20 20:33:11 +0100

    Add files via upload

---
+++ Application.java
@@ -0,0 +1,10 @@
+package com.tokioschool.basedatos.dao;
+
+public class Application {
+    public static void main (String[]args){
+
+        Taller taller = new Taller();
+        taller.launch();
+
+    }
+}
